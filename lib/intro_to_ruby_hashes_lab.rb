766190c730a60db227a27792d596a3bb96587ba8
Jack Sanders <guitarbreakerjs@gmail.com>
2020-09-01 01:23:45 +0000

    Done.

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,16 +1,16 @@
+require 'pry'
 def new_hash
-  # return an empty hash
+  new = {}
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  pets = { :mira => "cute", :kint => "orange", :mary => "boston" }
 end
-
+binding.pry
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  new = { name: "Grace Hopper"}
 end
-
 
 def id_hash_generator(number)
-  # return a hash with a key :id assigned to the provided number
+  { id: number }
 end
